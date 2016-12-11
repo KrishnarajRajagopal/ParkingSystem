b39d9cb3a21d1ef240b051ace7f659fe63824d84
KrishnarajRajagopal <krishnarajonnet@gmail.com>
2016-12-11 23:50:07 +0400

    Remove commented Lines

--- AutoParking/src/main/java/com/gojek/autopark/process/Driver.java
+++ AutoParking/src/main/java/com/gojek/autopark/process/Driver.java
@@ -43,50 +43,5 @@ public class Driver {
 				}
 			}
 		}
-
-		/*
-		 * parkingLot.createParkingSlot(6); String[] attributes = new String[2];
-		 * attributes[0] = "KA­01­HH­1234"; attributes[1] = "White";
-		 * parkingLot.parkVehicle(attributes);
-		 * 
-		 * String[] attributes1 = new String[2]; attributes1[0] =
-		 * "KA­01­HH­1235"; attributes1[1] = "Blue";
-		 * parkingLot.parkVehicle(attributes1);
-		 * 
-		 * String[] attributes2 = new String[2]; attributes2[0] =
-		 * "KA­01­HH­1734"; attributes2[1] = "Black";
-		 * parkingLot.parkVehicle(attributes2);
-		 */
-		// parkingLot.showStatus();
-
-		// parkingLot.releaseVehicle(2);
-
-		// parkingLot.showStatus();
-
-		/*
-		 * String[] attributes3 = new String[2]; attributes3[0] =
-		 * "KA­01­HH­1235"; attributes3[1] = "White";
-		 * parkingLot.parkVehicle(attributes3);
-		 */
-
-		// parkingLot.showStatus();
-
-		// TODO Need to convert the getegistrationNosByColour and
-		// getSlotNosByColour to Factory Method
-
-		/*
-		 * parkingLot.getegistrationNosByColour("White");
-		 * 
-		 * parkingLot.getegistrationNosByColour("Silver");
-		 * 
-		 * parkingLot.getSlotNosByColour("White");
-		 * 
-		 * parkingLot.getSlotNosByColour("Silver");
-		 * 
-		 * parkingLot.getSlotNosByRegNo("KA­01­HH­1734");
-		 * 
-		 * parkingLot.getSlotNosByRegNo("TN­01­HH­1734");
-		 */
-
 	}
 }
